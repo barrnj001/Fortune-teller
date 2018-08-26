4a9d2b529f098a06ae6141ba63f7a382dc8d380c
Njeri <steilemil@gmail.com>
2018-08-26 16:37:11 -0400

    WIP

--- src/com/fortuneteller/FortuneTellerApp.java
+++ src/com/fortuneteller/FortuneTellerApp.java
@@ -1,10 +1,34 @@
 package com.fortuneteller;
 
+import java.util.Scanner;
+
 public class FortuneTellerApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		System.out.println("A new vision befalls me. Tell me your first name!");
+		String firstName = input.nextLine();
+		
+		System.out.println("Good, good. Now, your last name");
+		String lastName = input.nextLine();
+		
+		System.out.println("Quickly, I don't have all day. Now don't be shy, how old are you?");
+		int userAge = input.nextInt();
+		input.nextLine();
+		
+		System.out.println("So, your name is " + firstName + " " + lastName + ", and you're " + userAge + " years old? Excellent");
+		
+		System.out.println("Excellent! Now, I need just a bit more information from you. What number is your birth month?");
+		int birthMonth = input.nextInt();
+		input.nextLine();
+		
+		System.out.println("Finally, we're getting somwhwere. If you'll be so kind, tell me you're favorite ROYGBIV color. Don't know? Ask for 'Help'");
+		String faveColor = input.nextLine();
+		
+		
+		
+		
 	}
 
 }
---
+++ src/com/fortuneteller/helpme.java
@@ -0,0 +1,17 @@
+package com.fortuneteller;
+
+public class helpme {
+	String Red;
+	String Orange;
+	String Yellow;
+	String Green;
+	String Blue;
+	String Indigo;
+	String Violet;
+
+
+	public String Help(){
+	
+	return Red + " "+ Orange + " "+ Yellow + " "+ Green + " "+ Blue + " "+ Indigo + " "+ Violet;
+	}
+}
