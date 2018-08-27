a048b1827de5b2719a8018ade1ad80383d225ed0
Njeri <steilemil@gmail.com>
2018-08-26 21:59:39 -0400

    Roygbiv loop

--- src/com/fortuneteller/FortuneTellerApp.java
+++ src/com/fortuneteller/FortuneTellerApp.java
@@ -10,27 +10,42 @@ public class FortuneTellerApp {
 		String Help = "Red, orange, yellow, green, blue, indigo, violet.";
 		System.out.println("A new vision befalls me. Tell me your first name!");
 		String firstName = input.nextLine();
-		
+
 		System.out.println("Good, good. Now, your last name");
 		String lastName = input.nextLine();
-		
+
 		System.out.println("Quickly, I don't have all day. Now don't be shy, how old are you?");
 		int userAge = input.nextInt();
 		input.nextLine();
-		
-		System.out.println("So, your name is " + firstName + " " + lastName + ", and you're " + userAge + " years old? Excellent");
-		
-		System.out.println("Excellent! Now, I need just a bit more information from you. What number is your birth month?");
+
+		System.out.println(
+				"So, your name is " + firstName + " " + lastName + ", and you're " + userAge + " years old? Excellent");
+
+		System.out.println(
+				"Excellent! Now, I need just a bit more information from you. What number is your birth month?");
 		int birthMonth = input.nextInt();
 		input.nextLine();
 		
-		System.out.println("Finally, we're getting somwhwere. If you'll be so kind, tell me you're favorite ROYGBIV color. Don't know? Ask for 'Help'");
+		while (true) {
+
+		System.out.println("If you'll be so kind, tell me you're favorite ROYGBIV color. Don't know? Ask for 'Help'");
 		String faveColor = input.nextLine();
-		
-		if (faveColor.equals("Help")) {
+
+		if (faveColor.equals("Help") || faveColor.equals("help")) {
 			System.out.println(Help);
-		}
 		
+		} else {
+			System.out.println("Your obedience will be rewarded. Patience.");
+			System.out.println("How many siblings do you have?");
+
+			int sibNumber = input.nextInt();
+			input.nextLine();
+			
+			
+		break;
+		}
+		}
+
 	}
 
 }
